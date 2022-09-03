3e4e71d8cab04358c8a1a3a5b86b91cc5cce23b7
007anthony <anthony.pena@lernende.bbw.ch>
2022-09-03 23:24:33 +0200

    The Datamodel of the Room has to be implemented

---
+++ src/main/java/ch/bbw/ap/cluedo/Model/Room.java
@@ -0,0 +1,70 @@
+package ch.bbw.ap.cluedo.Model;
+
+import java.awt.*;
+
+public class Room {
+
+    private String name;
+    private int anzahlTueren;
+    private int anzahlFenster;
+    private String boden;
+    private Color wandFarbe;
+    private String moebel;
+
+    public Room(String name, int anzahlTueren, int anzahlFenster, String boden, Color wandFarbe, String moebel){
+        this.name = name;
+        this.anzahlTueren = anzahlTueren;
+        this.anzahlFenster = anzahlFenster;
+        this.boden = boden;
+        this.wandFarbe = wandFarbe;
+        this.moebel = moebel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAnzahlTueren() {
+        return anzahlTueren;
+    }
+
+    public void setAnzahlTueren(int anzahlTueren) {
+        this.anzahlTueren = anzahlTueren;
+    }
+
+    public int getAnzahlFenster() {
+        return anzahlFenster;
+    }
+
+    public void setAnzahlFenster(int anzahlFenster) {
+        this.anzahlFenster = anzahlFenster;
+    }
+
+    public String getBoden() {
+        return boden;
+    }
+
+    public void setBoden(String boden) {
+        this.boden = boden;
+    }
+
+    public Color getWandFarbe() {
+        return wandFarbe;
+    }
+
+    public void setWandFarbe(Color wandFarbe) {
+        this.wandFarbe = wandFarbe;
+    }
+
+    public String getMoebel() {
+        return moebel;
+    }
+
+    public void setMoebel(String moebel) {
+        this.moebel = moebel;
+    }
+}
