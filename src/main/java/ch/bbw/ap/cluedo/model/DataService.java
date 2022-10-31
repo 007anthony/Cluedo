a611a2991d0fd21670d099b3aec4deb5818e0bb4
007anthony <anthony.pena@lernende.bbw.ch>
2022-10-31 18:10:29 +0100

    had to add every weapon and scene

--- src/main/java/ch/bbw/ap/cluedo/model/DataService.java
+++ src/main/java/ch/bbw/ap/cluedo/model/DataService.java
@@ -21,7 +21,29 @@ public class DataService {
     new Person("Peacock", "Mrs", "weiblich", "Schmuck", Color.gray)
     );
 
+    private List<Weapon> weapons = List.of(
+            new Weapon("Candlestick", 40, 1500, "Dekoration", "schwer", "metal"),
+            new Weapon("Dagger", 20, 500, "Messer", "scharf", "metal"),
+            new Weapon("Lead Piping", 40, 300, "Rohr", "hard", "metal"),
+            new Weapon("Revolver", 17, 2000, "Schusswaffe", "schusskraft", "metal"),
+            new Weapon("Rope", 200, 1000, "Seil", "lang", "fasern"),
+            new Weapon("Spanner", 25.5, 2500, "Werkzeug", "schwer", "material")
+    );
+    private List<Scene> scenes = List.of(
+            new Scene("Hall", 3, 1, "kacheln", Color.white, "Tisch"),
+            new Scene("Lounge", 1, 5, "blauer Teppich", Color.white,
+                    "Sofa, Sessel, Tisch"),
+            new Scene("Dining Room", 2, 4, "Holz", Color.yellow,
+                    "Tisch, Komoden")
+    );
+
     public List<Person> getPersons() {
         return persons;
+    }
+    public List<Weapon> getWeapons() {
+        return weapons;
+    }
+    public List<Scene> getScenes() {
+        return scenes;
     }
 }
